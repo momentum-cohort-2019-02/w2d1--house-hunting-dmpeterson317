0e1ca58e5a5ae3fda0553942a7c66127b4ab3aed
dmpeterson317 <dmpeterson317@gmail.com>
2019-02-18 19:27:35 -0500

    learn how to use main method

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import constant
 import helper
 
-a = float(input("Enter your annual salary: "))
-p = float(input("Enter the percent of your salary to save, as a decimal: "))
-t = float(input("Enter the cost of your dream home: "))
+def main():
 
-helper.months_to_pay_for_house(a, p, t)
+	a = float(input("Enter your annual salary: "))
+	p = float(input("Enter the percent of your salary to save, as a decimal: "))
+	t = float(input("Enter the cost of your dream home: "))
 
-# helper.months_to_pay_for_house(50000, 0.25, 250000)
+	helper.months_to_pay_for_house(a, p, t)
+
+	# helper.months_to_pay_for_house(50000, 0.25, 250000)
+
+# Entry point into the program
+if __name__ == "__main__":
+	main()
